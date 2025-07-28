072419939b297cd83f22e92030be4ce03c730564
Shivang <shivangdubey731@gmail.com>
2025-07-28 09:35:27 +0530

    Create innit.py

---
+++ core/innit.py
@@ -0,0 +1,4 @@
+from .fetcher import extract_text_from_pdf, extract_text_from_txt
+from .summarizer import generate_summary
+from .quizgen import generate_flashcards, generate_quiz
+from .tts import text_to_speech
